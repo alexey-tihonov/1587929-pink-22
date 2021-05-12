acb0bd50108add32e732ac22da454da5f6605e51
alexey-tihonov <alexey.tihonov@travelline.ru>
2021-05-12 17:34:01 +0300

    Правка замечаний по базовым критериям

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,4 @@
+/* header */
 (function () {
   let header = document.querySelector('.header'),
       navToogle = document.querySelector('.nav-toggle');
@@ -11,4 +12,41 @@
     header.classList.toggle("header--closed");
     header.classList.toggle("header--opened");
   });
+})();
+
+/* map */
+(function () {
+  let coordinate = [59.938635, 30.323118];
+
+  let initMap = function (coordinate) {
+    ymaps.ready(function () {
+      let map = new ymaps.Map('map', {
+        center: coordinate,
+        zoom: 16,
+        controls: []
+      });
+
+      let placemark = new ymaps.Placemark(coordinate);
+      map.geoObjects.add(placemark);
+
+      let zoomControl = new ymaps.control.ZoomControl({
+        options: {
+          position: {
+            left: 10,
+            bottom: 50
+          }
+        }
+      });
+
+      map.behaviors.disable('scrollZoom');
+      map.controls.add(zoomControl);
+    });
+  };
+
+
+  try {
+    initMap(coordinate);
+  } catch(e) {
+    console.log(e);
+  }
 })();
